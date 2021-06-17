9776464b48d2af0b80586da6c16caea8dbbb5fd9
aurobindonayak249 <67051205+aurobindonayak249@users.noreply.github.com>
2021-06-17 21:30:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-CLI VIRTUAL ASSISTANT
-print("Hello!")
+#CLI VIRTUAL ASSISTANT
+
+def main():
+	run = True
+
+	while run:
+		command = input('>>')
+
+		if command =='Hi':
+			print('Hello!')
+		elif command == 'exit':
+			exit()
+		else:
+			print("couldn't process")
+
+if __name__ == '__main__':
+	main()
+
